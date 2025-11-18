fa8db4fcc840b5f352be18a4c6b0f11fe3d0737a
ItsHLa <nissanihala@gmail.com>
2025-11-18 12:33:08 +0200

    add M2M signals for category-book relationship handling

--- a_books/apps.py
+++ a_books/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ABooksConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'a_books'
+    
+    def ready(self) -> None:
+        import a_books.signals
---
+++ a_books/signals.py
@@ -0,0 +1,28 @@
+from django.dispatch import receiver
+from django.db.models.signals import pre_delete, post_save, m2m_changed
+from django.db.models import Count
+from a_books.models.book_model import Book, BookCategory
+from a_books.models.category_model import Category
+
+@receiver(m2m_changed, sender=Book.categories.through)
+def handle_category_removal(sender, instance, action, reverse, **kwargs):
+    if reverse:
+        books = Book.objects.annotate(
+            count = Count('categories')
+        ).filter(
+            categories=instance,
+            count = 1)
+        
+        books.delete()
+    if not reverse:
+        if instance.categories.count() == 0:
+            instance.delete()        
+
+@receiver(pre_delete, sender=Category)
+def handle_category_deletion(sender, instance, **kwargs):
+    books = Book.objects.annotate(
+            category_count = Count('categories')).filter(
+               categories = instance,
+                category_count=1)
+    books.delete()
+
